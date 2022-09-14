df8645ee1791ffbc208c3f908330dfd07a34a0cd
Adam DePaolo <amdepaolo@gmail.com>
2022-09-14 16:37:43 -0400

    commit tree fe39f2e1876ce29b7f5d71938f88aa907eb50a25

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index{|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+ elements.map{|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  desired_index = foods.index{|food| cheese_types.include?(food)}
+  if desired_index == nil
+    return nil
+  end
+  foods[desired_index]
 end
